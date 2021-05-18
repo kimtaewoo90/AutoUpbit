0ca4f42325b809ba7211233017dd14c808120cfc
kimtaewoo <tangbaboda@naver.com>
2021-05-19 04:57:51 +0900

    refactoring code

--- Strategy/Main.py
+++ Strategy/Main.py
@@ -1,3 +1,6 @@
+
+# Develop Branch
+
 import sys
 import pyupbit
 import time
@@ -12,14 +15,9 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QProgressBar, QTableWidge
 from PyQt5 import uic
 
 # Users package
-import Signals
+import Strategy1
+import config
 from functions import Utils
-
-# ----------- Global Variables ------------------ #
-
-global_ticker = "KRW-BTC"
-#------------------------------------------------ #
-
 
 
 class OrderbookWorker(QThread):
@@ -33,16 +31,17 @@ class OrderbookWorker(QThread):
 
     def run(self):
 
-        global global_ticker
+        #global global_ticker
 
         while self.alive:
 
-            self.GlobalTicker.emit(global_ticker)
-            orderbook = pyupbit.get_orderbook(str(global_ticker))
+            self.GlobalTicker.emit(config.global_ticker)
             
             time.sleep(0.1)
             try:
-                self.dataSent.emit(orderbook)
+                if config.global_ticker != "now running":
+                    orderbook = pyupbit.get_orderbook(str(config.global_ticker))
+                    self.dataSent.emit(orderbook)
             except:
                 print("None type orderbook")
 
@@ -54,309 +53,6 @@ class OrderbookWorker(QThread):
         self.start()
 
 
-class Bot(QThread):
-
-    Log = pyqtSignal(str)
-    
-    # Informations
-    GetTicker = pyqtSignal(str)
-    GetCurPrice = pyqtSignal(float)
-    Balance = pyqtSignal(float)
-    TotalPnL = pyqtSignal(float)
-    GetBuyCnt = pyqtSignal(str)  # 매수여부
-    ProfitTime = pyqtSignal(int)
-    LossTime = pyqtSignal(int)
-
-
-    # Signal1
-    GetSignal1 = pyqtSignal(bool)
-    GetFiveClose = pyqtSignal(float)
-    GetTargetPrice = pyqtSignal(float)
-
-    # Signal2
-    GetFiveOpen = pyqtSignal(float)
-    GetSignal2 = pyqtSignal(bool)
-
-    # Signal3
-    GetMAsignals = pyqtSignal(bool)
-    GetSignal3 = pyqtSignal(bool)
-
-
-    #TargetTicker = pyqtSignal(str)
-    BuyPrice = pyqtSignal(float)
-    CurPrice = pyqtSignal(float)            
-    TargetPrice = pyqtSignal(float)
-    LossCutPrice = pyqtSignal(float)
-    PnL = pyqtSignal(str)
-
-                          
-    def __init__(self):
-        super().__init__()
-        self.GlobalTicker = "KRW-BTC"
-
-        #self.running = True
-
-    def run(self):
-
-
-        # initialize params
-        target_price = 0
-        total_loss_time = 0
-        loss_time = 0
-        total_profit_time = 0
-        profit_time = 0
-        total_gain = 0
-        signal1 = False
-        signal2 = False
-        signal3 = False
-
-        # Global variables
-        global global_ticker
-        
-        noMoreVol = False # Get Tickers
-        #start_tag = True  # For Save result (make DataFrame for first time)
-        start_msg = True  # To send msg for first running bot
-        #five_mins = True  # To imporve performance
-
-        # declare user imports
-        util = Utils.UtilClass()
-        conn = Utils.Connection()
-        upbit = conn.ConnectToUpbit()
-    
-
-        #signals = Signals.Signals()
-        start_balance = upbit.get_balance("KRW")
-        
-        # start
-        while True:
-
-            if loss_time == 3:
-                sleep_time = 3600
-                util.SendMsg(f"loss count : {loss_time}\nsleep time : {sleep_time} sec")
-                time.sleep(sleep_time)
-                loss_time = 0
-                noMoreVol = False
-                #start_orderbook = False
-
-            # Monitoring the price for Buy coin
-            while True:
-                if noMoreVol is False:
-                    profit_time = 0
-                    self.Log.emit("Getting Target Ticker")
-                    global_ticker = util.GetVolume()
-                    Ticker = global_ticker
-                    self.Log.emit(f"Found Ticker, Target Coin is {Ticker}")
-                    start_msg = True
-                    bot_start = datetime.datetime.now()
-
-
-                noMoreVol = True    # GetVolume 중복 실행 방지
-
-                if start_msg is True:
-                    util.SendMsg(f"Monitoring price of {Ticker}")
-                    self.Log.emit(f"Monitoring price of {Ticker}")
-                    start_msg = False
-                    
-                try:
-                    res = util.GetTarget(Ticker)
-                    five_closed = res["close"]
-                    five_open = pyupbit.get_ohlcv(Ticker, "minute5")
-                    five_open = five_open.iloc[-1]
-                    five_open = five_open["open"]
-                    five_temp = five_closed
-                    cur_price = pyupbit.get_current_price(Ticker)
-                    time.sleep(0.5)
-                except:
-                    five_closed = five_temp
-
-                if not type(cur_price) == float:
-                    time.sleep(0.2)
-                    cur_price = pyupbit.get_current_price(Ticker)
-                    self.Log.emit("Error with Get current price")
-
-                buy_price = 0
-                sell_price = 0
-                balance = upbit.get_balance("KRW")
-                judge_ma = util.GetMA(Ticker, cur_price, 10, 5)
-                
-                target_price = round(five_closed * 1.005)
-
-                # Signals Class instance
-                signals = Signals.Signals()
-
-                # Get signals
-                signal1 = signals.signal1(cur_price, target_price)
-                signal2 = signals.signal2(five_open, five_closed)
-                signal3 = signals.signal3(judge_ma)
-
-                # Display
-                self.GetTicker.emit(Ticker)
-                self.GetCurPrice.emit(cur_price)
-                self.GetTargetPrice.emit(round(five_closed * 1.005))
-                self.GetFiveClose.emit(five_closed)
-                self.GetFiveOpen.emit(five_open)
-                self.GetSignal1.emit(signal1)
-                self.GetSignal2.emit(signal2)
-                self.GetSignal3.emit(signal3)
-                self.GetMAsignals.emit(judge_ma)
-                self.GetBuyCnt.emit("False")
-                self.Log.emit(f"[{datetime.datetime.now()}] : Monitoring {Ticker}")
-                self.Balance.emit(balance)
-
-                print(f"Target Coin : {Ticker} / Judge MA : {judge_ma} / five_closed : {five_closed} now_five_open : {five_open} / Current_price : {cur_price} / Target_buy_price : {target_price}")
-
-                # 1시간동안 매수가 없으면 티커 다시 찾기
-                if bot_start - datetime.datetime.now() > datetime.timedelta(hours=1):
-                    noMoreVol = False
-
-                # Buy the coin
-                if signal1 is True and signal2 is True and signal3 is True:
-                    
-                    buy_amt = balance - math.ceil(balance * 0.05)
-                    buy_price = cur_price
-                    #resp = upbit.buy_market_order(Ticker, buy_amt)
-                    upbit.buy_market_order(Ticker, buy_amt)
-                    print(f"Success to Buy {Ticker} at {buy_price}")
-
-                    # 매수여부 Display
-                    self.GetBuyCnt.emit("True")
-
-                    util.SendMsg(
-                    f"""!Success to Buy!\nTicker : {Ticker}\nBuy Price : {buy_price}\nTargetPrice : {round(buy_price * 1.02)}\nLossCut Price : {math.ceil(buy_price*0.975)}
-                    """)
-                    self.Log.emit( f"""!Success to Buy!\nTicker : {Ticker}\nBuy Price : {buy_price}\nTargetPrice : {round(buy_price * 1.02)}\nLossCut Price : {math.ceil(buy_price*0.975)}
-                    """)
-
-                    
-                    # Monitoring the price for Sell coin
-                    while True:
-                        try:
-                            time.sleep(0.5)
-                            losscut_rate = 0.98
-                            cur_price_to_sell = pyupbit.get_current_price(Ticker)
-                            print(f"[{datetime.datetime.now()}]: Ticker : {Ticker} / Buy_Price : {buy_price} / Current_Price : {cur_price_to_sell} / Target_Sell_Price : {round(buy_price * 1.02)} / LosCut_Sell_Price : {math.ceil(buy_price*losscut_rate)} / PnL : {format((cur_price_to_sell - buy_price)/buy_price * 100, '.2f')} %" )
-                            #self.Log.emit(f"[{datetime.datetime.now()}]: Ticker : {Ticker} / Buy_Price : {buy_price} / Current_Price : {cur_price_to_sell} / Target_Sell_Price : {round(buy_price * 1.02)} / LosCut_Sell_Price : {math.ceil(buy_price*0.975)} / PnL : {format((cur_price_to_sell - buy_price)/buy_price * 100, '.2f')} %" )
-
-                            monitoring_pnl = f"{format((cur_price_to_sell - buy_price)/buy_price * 100, '.2f')} %"
-                            self.GetCurPrice.emit(cur_price_to_sell)
-                            self.BuyPrice.emit(buy_price)
-                            self.TargetPrice.emit(round(buy_price * 1.02))
-                            self.LossCutPrice.emit(math.ceil(buy_price * losscut_rate))
-                            self.PnL.emit(monitoring_pnl)
-
-                            # Sell the coin
-                            if cur_price_to_sell >= math.ceil(buy_price * 1.02) or cur_price_to_sell <= math.ceil(buy_price * losscut_rate):# or sell_timing is True:
-                                remained_coin = upbit.get_balance(Ticker)
-                                resp_sell = upbit.sell_limit_order(Ticker, cur_price_to_sell, remained_coin)
-                                sell_price = cur_price_to_sell
-                                uuid = resp_sell["uuid"]
-                                print(uuid)
-                                util.SendMsg("waiting for Limit Short Orders")
-                                print("waiting for Limit Short Orders")
-                                self.Log.emit("waiting for Limit Short Orders")
-
-                                sell_start_time = time.time()
-                                while True:
-                                    state = upbit.get_order(Ticker)
-                                    waiting_sell_time = time.time()
-
-                                    # 수동매도 시 break
-                                    #if upbit.get_balance(Ticker) == 0:
-                                    #    self.Log.emit("수동매도 되었습니다.")
-                                    #    break
-                                    #print(f"waiting_sell_time - sell_start_time = {waiting_sell_time - sell_start_time}")
-                                    
-                                    # 계속 지정가 매도가 체결이 안되면 취소하고 시장가로 매도 하기
-                                    if waiting_sell_time - sell_start_time > 60:
-                                        util.SendMsg("시장가 매도 주문 시작")
-                                        upbit.cancel_order(uuid)
-                                        time.sleep(1)
-                                        #resp_market_sell = upbit.sell_market_order(Ticker, remained_coin)
-                                        upbit.sell_market_order(Ticker, remained_coin)
-                                        self.Log.emit("시장가 매도 후 잔고 변경중(5sec)")
-                                        time.sleep(5)
-                                        res_balance = upbit.get_balance("KRW")
-                                        total_gain = total_gain + (res_balance - balance)
-                                        util.SendMsg(
-                                        f"""시장가 매도 체결 결과\nStart Balance : {round(balance)}\nEnd Balance : {round(res_balance)}\nGain : {round(res_balance - balance)}\nTotal PnL : {round(total_gain)}
-                                        """)
-                                        self.Log.emit(f"""시장가 매도 체결 결과\nStart Balance : {round(balance)}\nEnd Balance : {round(res_balance)}\nGain : {round(res_balance - balance)}\nTotal PnL : {round(total_gain)}
-                                        """)
-                                        self.Balance.emit(res_balance)
-                                        self.TotalPnL.emit(res_balance - start_balance)
-                                        df = pyupbit.get_ohlcv(Ticker, "minute5")
-                                        five_bong = df.iloc[-1]
-                                        sell_five_bong = five_bong.name                                    
-                                        break
-
-                                    # 지정가 매도 성공시
-                                    if len(state) == 0:
-                                        res_balance = upbit.get_balance("KRW")
-                                        time.sleep(1)
-                                        total_gain = total_gain + (res_balance - balance)
-                                        util.SendMsg(
-                                        f"""지정가 매도 체결 결과\nSell price : {sell_price}\nBought price : {buy_price}\nPnL : {format((sell_price - buy_price)/buy_price * 100, '.2f')} %\nStart Balance : {round(balance)}\nEnd Balance : {round(res_balance)}\nGain : {round(res_balance - balance)}\nTotal PnL : {round(total_gain)}
-                                        """)
-                                        self.Log.emit(f"""지정가 매도 체결 결과\nSell price : {sell_price}\nBought price : {buy_price}\nPnL : {format((sell_price - buy_price)/buy_price * 100, '.2f')} %\nStart Balance : {round(balance)}\nEnd Balance : {round(res_balance)}\nGain : {round(res_balance - balance)}\nTotal PnL : {round(total_gain)}
-                                        """)
-                                        self.Balance.emit(res_balance)
-                                        self.TotalPnL.emit(res_balance - start_balance)
-
-                                        df = pyupbit.get_ohlcv(Ticker, "minute5")
-                                        five_bong = df.iloc[-1]
-                                        sell_five_bong = five_bong.name
-                                        break
-                                
-                                # 시장가 또는 지정가 매도가 체결 된 후 profit 인지 loss 인지 판단 & 알림
-                                if sell_price > buy_price:
-                                    profit_time = profit_time + 1
-                                    total_profit_time = total_profit_time + 1
-                                    
-                                    util.SendMsg(f"Profit count : [ {total_profit_time} ]")
-                                    self.Log.emit(f"Profit count : [ {total_profit_time} ]")
-
-                                elif sell_price <= buy_price:
-                                    loss_time = loss_time + 1
-                                    total_loss_time = total_loss_time + 1
-
-                                    util.SendMsg(f"loss count : [ {loss_time} ]")
-                                    self.Log.emit(f"loss count : [ {loss_time} ]")
-
-                                    noMoreVol = False
-                                #SaveResult(profit_time, loss_time, total_gain, start_tag)
-
-                                self.ProfitTime.emit(total_profit_time)
-                                self.LossTime.emit(total_loss_time)
-
-                                # 매도 완료시 시그널 False로 초기화.
-                                #self.Getsignal1.emit(False)
-                                #self.Getsignal2.emit(False)
-                                #self.Getsignal3.emit(False)
-                                #self.BuyPrice.emit(0.0)
-                                #self.TargetPrice.emit(0.0)
-                                #self.LossCutPrice.emit(0.0)
-                            
-
-                                # 매도시의 5분봉과 같은 5분봉에서 재매수 방지
-                                while True:
-                                    if datetime.datetime.now() - sell_five_bong > datetime.timedelta(minutes=5):
-                                        util.SendMsg("Find signals in new Five scalping\n")
-                                        self.Log.emit("Find signals in new Five scalping\n")
-                                        break
-                                break
-                        except:
-                            print("error..")
-                            #continue 
-                break
-
-
-
-    #def resume(self):
-    #    self.running = True
-
-    #def pause(self):
-    #    self.running = False
-
 main_ui = uic.loadUiType("autoUpbit_v2.ui")[0]
 
 class MainWindows(QMainWindow, main_ui):
@@ -365,7 +61,7 @@ class MainWindows(QMainWindow, main_ui):
         super().__init__()
         self.setupUi(self)
 
-        self.bot = Bot()
+        self.bot = Strategy1.Bot()
         self.bot.Log.connect(self.Log)
         self.bot.Balance.connect(self.Balance)
         self.bot.TotalPnL.connect(self.TotalPnL)
